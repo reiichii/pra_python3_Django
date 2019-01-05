968b73f3cee0dfe0e8ae87f39bfd80a366d586ec
reiichii <passion.engk@gmail.com>
2019-01-05 16:52:23 +0900

    create application systems

---
+++ item/forms.py
@@ -0,0 +1,10 @@
+from django.forms import ModelForm
+from .models import Item # このドットは何
+
+class ItemForm(ModelForm):
+    class Meta:
+        model = Item
+        fields = [
+            'name', 'image_url', 'rating', 'piece_count',
+            'minifig_count', 'us_price', 'owner_count', 'want_it_count'
+        ]
--- item/views.py
+++ item/views.py
@@ -1,5 +1,12 @@
 from django.contrib.auth.decorators import login_required
-from django.template.response import TemplateResponse
+from django.template.response import TemplateResponse, HttpResponse
+from django.views.decorators.http import require_POST
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+
+from .forms import ItemForm
+from .models import Item, WishList
 
 # ビュー
 # def hello(request):
@@ -13,3 +20,62 @@ from django.template.response import TemplateResponse
 def edit(request, item_id):
 
     item = get_object_or_404(Item, id=item_id)
+
+    if request.method == 'POST':
+        form = ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('item_index'))
+    else:
+        form = ItemForm(instance=item)
+
+    context = {'form': form, 'item': item}
+    return TemplateResponse(request, 'item/edit.html', context=context)
+
+
+@login_required
+@require_POST
+def delete(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+
+    item.delete()
+
+    return HttpResponseRedirect(reverse('item_index'))
+
+
+@login_required
+def index(request):
+    context = {'items': Item.objects.all()}
+    return TemplateResponse(request, 'item/list.html', context=context)
+
+
+@login_required
+@require_POST
+def add_to_wish_list(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+
+    wish_list, created = WishList.objects.get_or_create(user=request.user)
+
+    wish_list.items.add(item)
+
+    return HttpResponseRedirect(reverse('wish_list_index'))
+
+
+@login_required
+@require_POST
+def delete_from_wish_list(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+
+    wish_list, created = WishList.objects.get_or_create(user=request.user)
+
+    wish_list.items.remove(item)
+
+    return HttpResponseRedirect(reverse('wish_list_index'))
+
+
+@login_required
+def wish_list_index(request):
+    wish_list, created = WishList.objects.get_or_create(user=request.user)
+
+    context = {'items': wish_list.items.all()}
+    return TemplateResponse(request, 'wish_list/list.html', context=context)
